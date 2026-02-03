2fda391ab4a0e3473ee8c175b2086d598f02602d
bcunnane <brandon.cunnane@gmail.com>
2026-02-02 22:53:42 -0500

    combined scrape ratings and scrape movielist

--- update_db.py
+++ update_db.py
@@ -1,21 +1,12 @@
-import os
-import requests
-import sqlite3
-from bs4 import BeautifulSoup
-from time import sleep, strptime
-import random
-
+import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from webdriver_manager.chrome import ChromeDriverManager
-import pandas as pd
-import time
 
 # constants
-START_DATE = strptime('2024-12-01','%Y-%m-%d')
 URLS = {'movies': 'https://letterboxd.com/_branzino/list/oscars-2026/',
         'oscars':'https://letterboxd.com/000_leo/list/oscars-2026-1/'}
 AWARD_NUM = {'oscars':10}
@@ -47,13 +38,16 @@ def setup_driver():
     return webdriver.Chrome(service=service, options=options)
 
 
-def scrape_ratings(initials, url):
-    '''updates database with user movie ratings'''
+def scrape(type, label, url):
+    '''webscrape letterboxd film data'''
     #initialize
-    ratings = pd.DataFrame(columns=['initials','filmid','rating'])
+    movielist = pd.DataFrame(columns=[type,'filmid','rating'])
     movies = pd.DataFrame(columns=['filmid','title', 'slug'])
 
-    # scrape
+    # poster scraping per type
+    scraping = {'user':"griditem", 'list':"posteritem"}
+
+    # scrape posters
     driver = setup_driver()
     driver.get(url)
 
@@ -63,52 +57,10 @@ def scrape_ratings(initials, url):
             EC.presence_of_element_located((By.CLASS_NAME, "film-poster"))
         )
     except Exception:
-        print(f"ERROR: {url}")
+        print(f"ERROR: class film-poster not present at {url}")
         return None
 
-    posters = driver.find_elements(By.CLASS_NAME, "griditem")
-
-    # get rating for all posters in the page
-    for poster in posters:
-
-        # get movie data
-        filmid = int(poster.find_element(By.CLASS_NAME, "react-component").get_attribute("data-film-id"))
-        react_data = poster.find_element(By.CLASS_NAME, "react-component")
-        slug = react_data.get_attribute("data-item-slug")
-        title = react_data.get_attribute("data-item-name")
-        # date = movie.find_element(By.TAG_NAME, "time").get_attribute("datetime")[0:10]
-        try:
-            rating = poster.find_element(By.CLASS_NAME, "rating").text.strip()
-            rating = rating.count('★') + 0.5 * rating.count('½')
-        except:
-            rating = 0
-
-        ratings.loc[len(ratings)] = {'initials':initials, 'filmid':filmid, 'rating':rating}
-        movies.loc[len(movies)] = {'filmid':filmid, 'title':title, 'slug':slug}
-        
-    return ratings, movies
-
-
-def scrape_movielist(year, url):
-    '''updates database with movies on watchlist'''
-    #initialize
-    movielist = pd.DataFrame(columns=['year','filmid'])
-    movies = pd.DataFrame(columns=['filmid','title', 'slug'])
-
-    # scrape
-    driver = setup_driver()
-    driver.get(url)
-
-    try:
-        # Wait for the page content to load
-        WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.CSS_SELECTOR, "ul.poster-list"))
-        )
-    except Exception:
-        print(f"ERROR: {url}")
-        # return None
-
-    posters = driver.find_elements(By.CLASS_NAME, "posteritem")
+    posters = driver.find_elements(By.CLASS_NAME, scraping[type])
 
     # get rating for all posters in the page
     for poster in posters:
@@ -118,15 +70,26 @@ def scrape_movielist(year, url):
         filmid = int(react_data.get_attribute("data-film-id"))
         slug = react_data.get_attribute("data-item-slug")
         title = react_data.get_attribute("data-item-name")
+        # date = movie.find_element(By.TAG_NAME, "time").get_attribute("datetime")[0:10]
+        try:
+            rating = poster.find_element(By.CLASS_NAME, "rating").text.strip()
+            rating = rating.count('★') + 0.5 * rating.count('½')
+        except:
+            rating = 0
 
-        movielist.loc[len(movielist)] = {'year':year, 'filmid':filmid}
+        movielist.loc[len(movielist)] = {type:label, 'filmid':filmid, 'rating':rating}
         movies.loc[len(movies)] = {'filmid':filmid, 'title':title, 'slug':slug}
         
-    # quit driver to avoid bot detector
+    # remove reting from movielist
+    if type == 'list':
+        movielist.drop(columns=['rating'], inplace=True)
+
+
+    # quit driver to avoid bot detection
     driver.quit()
 
     return movielist, movies
-    
+
 
 def main():
     '''
@@ -145,12 +108,10 @@ def main():
     # all_movies = pd.read_csv('data\\movies.csv')
     
     # page = 1
-    # user = USERS[0]
+    # user = USERS[5]
     # url = f'https://letterboxd.com/{user[1]}/films/by/date/page/{page}/'
     
-    # # all_ratings, all_movies = scrape_ratings(user[0], url)
-
-    # new_ratings, new_movies = scrape_ratings(user[0], url)
+    # new_ratings, new_movies = scrape('user', user[0], url)
     # all_ratings = pd.concat([new_ratings, all_ratings]).drop_duplicates()
     # all_movies = pd.concat([new_movies, all_movies]).drop_duplicates()
 
@@ -160,18 +121,15 @@ def main():
     URLS = {'movies': 'https://letterboxd.com/_branzino/list/oscars-2026/',
             'oscars':'https://letterboxd.com/000_leo/list/oscars-2026-1/'}
 
-    noms, movies = scrape_movielist('2025', 'https://letterboxd.com/000_leo/list/oscars-2026-1/')
+    # all_noms = pd.read_csv('data\\noms.csv')
+    all_movies = pd.read_csv('data\\movies.csv')
 
+    new_noms, new_movies = scrape('list','2026', 'https://letterboxd.com/000_leo/list/oscars-2026-1/')
 
-    noms.to_csv('data\\noms.csv', index=False)
+    all_movies = pd.concat([new_movies, all_movies]).drop_duplicates()
 
-
-    # get movie data
-    # scrape_movies(cur, 'movies')
-    # if URLS['oscars']:
-    #     scrape_movies(cur, 'oscars')
-    #     sleep(3) # slow down scraping to avoid being blocked by letterboxd
-
+    new_noms.to_csv('data\\noms.csv', index=False)
+    all_movies.to_csv('data\\movies.csv', index=False)
 
 
 if __name__ == '__main__':
