39be9579bd52ba0a7575e1753296f7eb78f40db9
bcunnane <brandon.cunnane@gmail.com>
2026-09-14 15:16:48 -0400

    removed movies.csv

--- update_readme.py
+++ update_readme.py
@@ -13,55 +13,45 @@ def filmids_to_posters(df):
     for index,row in df.iterrows():
         slug = row['slug']
         posters.append(f'''<img src="https://a.ltrbxd.com/resized/film-poster/{'/'.join(row['filmid'])}/{row['filmid']}-{slug}-0-1000-0-1500-crop.jpg" alt="{row['slug']}" style="height: 105px; width:70px;"/>''')
-    
+
     df['filmid'] = posters
-    df.rename(columns={"filmid": "Movie"}, inplace=True)
     return df
 
 
 def main():
 
-    # import movie data
-    movies = pd.read_csv('data\\movies.csv')
+    # import data
     ratings = pd.read_csv('data\\ratings.csv')
-    ratings = movies.merge(ratings, how='inner', on='filmid')
-
-    # import year-specific data
-    year = 2026
-    # noms = pd.read_csv('data\\noms.csv')
-    # noms = noms[noms['list'] == year][['filmid', 'best_pic']]
-    # watchlist = pd.read_csv('data\\watchlist.csv')
-    # watchlist = watchlist[watchlist['list'] == year]
+    watchlist = pd.read_csv('data\\watchlist.csv')
+    noms = pd.read_csv('data\\noms.csv')
 
     # apply year data to movie data
-    # ratings = ratings.merge(watchlist, how='inner', on='filmid')
-    # ratings = ratings.merge(noms, how='left', on='filmid')
+    ratings = ratings.merge(watchlist, how='inner', on='slug')
+    ratings = ratings.merge(noms, how='left', on='slug')
 
     # get leaderboard data
     total = ratings.groupby('user')['user'].count()
-    # best_pic = ratings[ratings['best_pic']==1].groupby('user')['user'].count()
-    # oscar_pct = 100 * ratings[ratings['best_pic'].notna()].groupby('user')['user'].count() / len(noms)
+    best_pic = ratings[ratings['best_pic']==1].groupby('user')['user'].count()
+    oscar_pct = 100 * ratings[ratings['best_pic'].notna()].groupby('user')['user'].count() / len(noms)
 
     # rename leaderboard columns
     total.rename('Total', inplace=True)
-    # best_pic.rename('Best Pics', inplace=True)
-    # oscar_pct.rename('Oscar %', inplace=True)
+    best_pic.rename('Best Pics', inplace=True)
+    oscar_pct.rename('Oscar %', inplace=True)
 
     # compile leaderboard
     leader = pd.concat([
         total
-        # , best_pic
-        # , oscar_pct.astype(int)
+        , best_pic
+        , oscar_pct.astype(int)
     ], axis=1)
-    # leader.loc['MF','Total'] += 3 # shorts
-    # leader.loc['NB','Total'] += 3 # shorts
     leader = leader.sort_values(by='Total', ascending=False)
     leader.rename_axis("Name", axis=0, inplace=True)
     leader = leader.to_markdown()
 
     # compile aggregate movie data
     non_zero_ratings = ratings[ratings['rating']>0]
-    agg_movie_data = non_zero_ratings.groupby(['filmid', 'slug'])['rating'].agg(
+    agg_movie_data = non_zero_ratings.groupby(['slug'])['rating'].agg(
         Std='std',
         Min='min',
         Ave='mean',
@@ -69,7 +59,8 @@ def main():
         Views='count'
     ).reset_index()
     agg_movie_data = agg_movie_data[agg_movie_data['Views'] > 2] # must have 3 ratings
-    agg_movie_data = filmids_to_posters(agg_movie_data)
+    # agg_movie_data = filmids_to_posters(agg_movie_data)
+    agg_movie_data.rename(columns={"slug": "Movie"}, inplace=True)
 
     # get best movies
     best_movies = agg_movie_data[agg_movie_data['Ave'] >= 3.0]
@@ -95,23 +86,23 @@ def main():
 
     # get watched
     watched = ''
-    # watched = ratings[['user', 'slug', 'rating']].copy()
-    # watched.rename(columns={'user': 'Name', 'slug':'Movie', 'rating':'Rating'}, inplace=True)
-    # watched['Rating'] = watched['Rating'].astype(str)
+    watched = ratings[['user', 'slug', 'rating']].copy()
+    watched.rename(columns={'user': 'Name', 'slug':'Movie', 'rating':'Rating'}, inplace=True)
+    watched['Rating'] = watched['Rating'].astype(str)
 
-    # # remove 0 ratings aka "watched" moves
-    # watched.loc[watched['Rating'] == '0.0', 'Rating'] = 'X'
+    # remove 0 ratings aka "watched" moves
+    watched.loc[watched['Rating'] == '0.0', 'Rating'] = 'X'
 
-    # # create pivot table
-    # watched = watched.pivot(index='Movie', columns='Name', values='Rating')
-    # watched[watched.isnull()] = ''
+    # create pivot table
+    watched = watched.pivot(index='Movie', columns='Name', values='Rating')
+    watched[watched.isnull()] = ''
     
-    # # split table into groups of n movies
-    # n = 8
-    # watched = [watched.iloc[i:i+n].to_markdown(floatfmt=".1f") for i in range(0, watched.shape[0], n)]
+    # split table into groups of n movies
+    n = 8
+    watched = [watched.iloc[i:i+n].to_markdown(floatfmt=".1f") for i in range(0, watched.shape[0], n)]
 
     # update README.md
-    output = f'''Aggregate Letterboxd movie ratings for {year}! <br />
+    output = f'''Aggregate Letterboxd movie ratings for 2026! <br />
 Last updated on {datetime.datetime.now().strftime('%a %b %d at %I:%M %p')} <br />
 Watchlist can be found [here](https://letterboxd.com/_branzino/list/oscars-2026/)
 
