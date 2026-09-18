28c571422fd18cce82951b315cffc1fefdd705d9
bcunnane <brandon.cunnane@gmail.com>
2026-09-17 23:01:43 -0400

    fixed style html

--- update_readme.py
+++ update_readme.py
@@ -142,47 +142,25 @@ def main():
 Last updated on {datetime.datetime.now().strftime('%a %b %d at %I:%M %p')} <br />
 Watchlist can be found [here](https://letterboxd.com/_branzino/list/oscars-2026/)
 
-<style>
-/* Custom styling to make horizontal scrollbars always visible and clean */
-.scrollable-table {{
-    overflow-x: auto;
-    margin-bottom: 20px;
-}}
-.scrollable-table::-webkit-scrollbar {{
-    height: 8px;
-}}
-.scrollable-table::-webkit-scrollbar-track {{
-    background: #f1f1f1;
-    border-radius: 4px;
-}}
-.scrollable-table::-webkit-scrollbar-thumb {{
-    background: #888;
-    border-radius: 4px;
-}}
-.scrollable-table::-webkit-scrollbar-thumb:hover {{
-    background: #555;
-}}
-</style>
-
 ## Leaderboard :trophy:
 {leader}
 
 ## Loved Movies :heart:
-<div style="overflow-x: auto;">
+<div style="overflow-x: auto; -webkit-overflow-scrolling: touch; margin-bottom: 20px;">
 
 {best_movies}
 
 </div>
 
 ## Unloved Movies :broken_heart:
-<div style="overflow-x: auto;">
+<div style="overflow-x: auto; -webkit-overflow-scrolling: touch; margin-bottom: 20px;">
 
 {worst_movies}
 
 </div>
 
 ## Controversial Movies :hot_pepper:
-<div style="overflow-x: auto;">
+<div style="overflow-x: auto; -webkit-overflow-scrolling: touch; margin-bottom: 20px;">
 
 {controversial}
 
@@ -192,9 +170,9 @@ Watchlist can be found [here](https://letterboxd.com/_branzino/list/oscars-2026/
 {critics}
 
 ## All Watched :movie_camera:
-<div  style="overflow-x: scroll;">
+<div style="overflow-x: auto; -webkit-overflow-scrolling: touch; margin-bottom: 20px;">
 
-{'\n\n</div>\n\n<div  style="overflow-x: scroll;">\n\n'.join(watched)}
+{'\n\n</div>\n\n<div style="overflow-x: auto; -webkit-overflow-scrolling: touch; margin-bottom: 20px;">\n\n'.join(watched)}
 
 </div>
 
